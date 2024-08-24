dbddc5add1b450e3e62bf2a2d50dca7335316bb6
Subhashis <subhashisdhara123@gmail.com>
2024-08-24 12:40:38 +0530

    added

--- classes_and_oop/getter_setter.js
+++ classes_and_oop/getter_setter.js
@@ -4,17 +4,25 @@ class User {
         this.password = password
     }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
     get password(){
-        return this.password.toUpperCase()
+        return this._password.toUpperCase()
     }
 
     set password(value){
-        this.password = value
+        this._password = value.toUpperCase()
     }
 }
 
-const hitesh = new User("hitesh@google.com", "1234")
-console.log(hitesh.password)
+const hitesh = new User("hitesh@google.com", "1234abc")
+// console.log(hitesh.password)
+console.log(hitesh.email);
 
-hitesh.password = "12345"
-console.log(hitesh.password)
+
